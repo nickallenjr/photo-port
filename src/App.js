06b53a2aa453faacaeafea4ef2b670b9f4eeb271
khaoula <khaoulajabour4@gmail.com>
2022-03-03 01:42:07 -0500

    control state with a controlled component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState} from 'react';
 import Nav from "./components/Nav";
 import About  from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 import './App.css';
 
 // import logo from './logo.svg';
@@ -37,6 +38,7 @@ const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
       <main>
         <div>
+        <ContactForm/>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
         </div>
